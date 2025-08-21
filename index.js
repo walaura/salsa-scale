22b88245d8ef84fe414979e9acc6649aa4c32c36
Laura buns <hi@laura.monster>
2025-08-21 09:48:39 +0000

    dsgfdfg

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/track/" + process.env.TOP_SECRET_PATH + "/:weight", (req, res) => {
+  const weight = req.params.weight;
+  const unixDate = Date.now();
+  console.log(`Weight: ${weight}, Timestamp: ${unixDate}`);
+  res.send(`Weight received: ${weight}`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
