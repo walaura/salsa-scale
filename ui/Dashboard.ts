aa892875ad560d84384d425fbae41daa0793027a
Laura buns <hi@laura.monster>
2025-09-05 14:29:57 +0000

    lololol

--- ui/Dashboard.ts
+++ ui/Dashboard.ts
@@ -1,6 +1,7 @@
 import type { WithId } from "mongodb";
 import type { LogEntry } from "../app/setup/db.ts";
 import { formatGrams, formatTime, formatTimeHtml } from "../app/format.ts";
+import { withStyles, css } from "../app/setup/styles.ts";
 
 const makeDashboardWidget = ({
   title,
@@ -12,11 +13,11 @@ const makeDashboardWidget = ({
   icon: string;
 }) => {
   return /* HTML */ `
-    <div class="dashboard-widget">
+    <div class="${className("widget")}">
       <strong>${title}</strong>
-      <span class="dashboard-widget-text-w-icon">
-        <img src="./static/${icon}.gif" alt="" class="dashboard-icon" />
-        <div class="dashboard-fact">${fact}</div>
+      <span class="${className("widget-text-w-icon")}">
+        <img src="./static/${icon}.gif" alt="" class="${className("icon")}" />
+        <div class="${className("fact")}">${fact}</div>
       </span>
     </div>
   `;
@@ -49,7 +50,7 @@ const makeDashboard = ({
     now - timeSinceLast + averageTimeBetweenEvents;
 
   return /* HTML */ `
-    <div class="dashboard">
+    <div class="${className}">
       ${makeDashboardWidget({
         title: "Time since last event",
         fact: formatTime(timeSinceLast, true),
@@ -75,5 +76,41 @@ const makeDashboard = ({
     </div>
   `;
 };
+
+const [className] = withStyles(
+  (root) => css`
+    ${root} {
+      display: grid;
+      background: var(--neutral-0-A80);
+      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+      margin: -0.5px;
+
+      ${root("widget")} {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.5rem;
+        flex-direction: column;
+        border: 1px solid var(--pink-200);
+        margin: -0.5px;
+        padding: 2rem 1rem;
+        strong {
+          font-size: var(--font-secondary);
+          color: var(--pink-600);
+          font-weight: inherit;
+          font-style: italic;
+        }
+        ${root("widget-text-w-icon")} {
+          display: flex;
+          align-items: center;
+          gap: 0.25rem;
+        }
+        ${root("fact")} {
+          font-weight: 800;
+        }
+      }
+    }
+  `
+);
 
 export { makeDashboard };
