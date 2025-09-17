bc5fe20ddf0a0101e411eff9ec683a37a8bd67cb
Laura buns <hi@laura.monster>
2025-09-17 06:23:10 +0000

    sdfdsf

--- scripts/bundleStyles.ts
+++ scripts/bundleStyles.ts
@@ -5,15 +5,19 @@ import {
 } from "local-css/helpers";
 import path from "path";
 import { mkdir, writeFile } from "fs/promises";
+import prettier from "prettier";
 
 ROUTES;
 
 mkdir(BUILD_CACHE_DIR, { recursive: true });
 await Promise.all(
-  Array.from(getUnbundledRegisteredStyles()).map(([key, value]) =>
-    writeFile(path.join(BUILD_CACHE_DIR, `${key}.css`), value + "\n")
-  )
+  Array.from(getUnbundledRegisteredStyles()).map(async ([key, value]) =>
+    writeFile(
+      path.join(BUILD_CACHE_DIR, `${key}.css`),
+      (await prettier.format(value, { parser: "css" })) + "\n",
+    ),
+  ),
 );
 console.log(
-  `Wrote ${getUnbundledRegisteredStyles().size} styles to ${BUILD_CACHE_DIR}`
+  `Wrote ${getUnbundledRegisteredStyles().size} styles to ${BUILD_CACHE_DIR}`,
 );
